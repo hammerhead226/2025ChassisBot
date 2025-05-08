e12acb4acd29c2ffc1ef0eb1aa17a01c51bdc866
devinisaperson <177495008+devinisaperson@users.noreply.github.com>
2025-05-08 18:38:01 -0400

    copy paste in templetes

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "ChassisBotCode";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "15d886172f1ef4ef288c7603db2cd65b4975d4ef";
+  public static final String GIT_DATE = "2025-05-03 10:19:11 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-05-06 19:27:51 EDT";
+  public static final long BUILD_UNIX_TIME = 1746574071984L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
