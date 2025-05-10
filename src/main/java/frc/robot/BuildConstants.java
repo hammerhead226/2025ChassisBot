332aac8fb3c5c25aa8e09fe99880066bef434c3b
devinisaperson <177495008+devinisaperson@users.noreply.github.com>
2025-05-10 11:08:41 -0400

    new tuner constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025ChassisBot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "e12acb4acd29c2ffc1ef0eb1aa17a01c51bdc866";
-  public static final String GIT_DATE = "2025-05-08 18:38:01 EDT";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "a5cd6dbbb229cc989b5549d7e4686e7c61fd0757";
+  public static final String GIT_DATE = "2025-05-08 19:23:10 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-05-08 18:39:13 EDT";
-  public static final long BUILD_UNIX_TIME = 1746743953833L;
+  public static final String BUILD_DATE = "2025-05-10 11:04:47 EDT";
+  public static final long BUILD_UNIX_TIME = 1746889487201L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -118,7 +118,6 @@ public class Robot extends LoggedRobot {
     // Return to non-RT thread priority (do not modify the first argument)
     // Threads.setCurrentThreadPriority(false, 10);
 
-    
   }
 
   /** This function is called once when the robot is disabled. */
